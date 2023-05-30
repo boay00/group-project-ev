761d56d660b664bf90cb63ef043069e88e5a1148
Bede Young <bedeyoung19@gmail.com>
2023-05-30 21:56:53 +0100

    app update

--- interactive-map-app.py
+++ interactive-map-app.py
@@ -8,10 +8,19 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.markdown('## US States - Air Quality vs EV Charging Stations')
 
-year = st.slider("Year", min_value=2008, max_value=2022, value=2008)
-ev = (plot_heatmap_ev(year))
+slider_value = st.empty()
 
-st.pyplot(ev)
-st.pyplot(plot_heatmap_aq(year))
+year = slider_value.slider("Year", min_value=2008, max_value=2022, value=2008)
+button_year = st.button('Submit')
+
+# Check if the "Submit" button is clicked
+if button_year:
+    
+    st.pyplot(plot_heatmap_aq(year))
+
+    st.pyplot(plot_heatmap_ev(year))
+
+
+
 
 
