e900ecdf51ba8eb13833e2db673523d41e7669a3
Bede Young <bedeyoung19@gmail.com>
2023-06-12 21:45:07 +0100

    app update

--- interactive-map-app.py
+++ interactive-map-app.py
@@ -8,7 +8,18 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.markdown('''## US States - Air Quality vs EV Charging Stations
 ---
-    #### Bede Young''')
+#### Bede Young''')
+            
+st.markdown('''---
+This app is a demo of the heatmaps which were used to identify the states with the most pressing need of electric charging station installations.
+
+View the github repo here:
+> [EV-Air-Quality-Repo-Bede-Young](https://github.com/boay00/group-project-ev)
+- Use the slider to select the desired year.
+- :white_check_mark: Green heatmaps are indicating the number of charging stations per 10,000 population
+- :large_blue_circle: Blue heatmaps are indicating the percentage of days with air quality classed 'bad' anually (ie. polutant levels did not meet satisfactory levels)
+
+''')
 
 slider_value = st.empty()
 
@@ -22,7 +33,13 @@ if button_year:
 
     st.pyplot(plot_heatmap_ev(year))
 
-
-
-
+    st.markdown('---')
+    st.markdown('''
+    ## Observations
+    ---
+    - From these heatmaps, we can observe that the Eastern States have shown a proportional improvment in air quality (days classed 'bad')
+    - Whilst California led in number of charging stations by a considerable amount in 2008, the Eastern States have greatly improved the availability of charging for Electric Vehicles in the last 15 years.
+    - The states with the worst data for Air Quality are in the South West, including Arizona (AZ), Texas (TX) and New Mexico (NM)
+    - AZ and NM have also indicated small improvements in EV charging availability in comparison to the rest of the US.
+    ''')
 
